4cfaf9072c8305d50e35bbce15335c53b1c44a71
StevenCesario <steven.cesario@proton.me>
2026-04-25 14:55:34 +0200

    we cannot create an empty note

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -3,19 +3,26 @@ import React, { useState } from 'react'
 // It'd be cool to pass some sort of isFirstTimeVisit or isFirstNote prop to this component but I just can't get the logic to fully work in the parent component right now. Future quality of life improvement!
 const NewNote = ({ onCreate }) => {
   const [newText, setNewText] = useState('');
+  const [isEmptyNoteError, setIsEmptyNoteError] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
-    onCreate(newText);
 
-    setNewText('');
+    if (newText.length === 0) {
+      setIsEmptyNoteError(true);
+    } else {
+      setIsEmptyNoteError(false);
+      onCreate(newText);
+
+      setNewText('');
+    }
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <textarea value={newText} onChange={(e) => setNewText(e.target.value)}></textarea>
       {/* <p>newText: {newText}</p> */}
-      <br />
+      {isEmptyNoteError && <p>Let's not create an empty note, shall we?</p>}
       <input type='submit' value='Create note!' />
     </form >
   )
