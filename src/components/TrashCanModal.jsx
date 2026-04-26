573737f41b452ae690b816be12f213510a8fe92f
StevenCesario <steven.cesario@proton.me>
2026-04-26 11:28:46 +0200

    future improvement idea

--- src/components/TrashCanModal.jsx
+++ src/components/TrashCanModal.jsx
@@ -9,6 +9,7 @@ const TrashCanModal = ({ notes, onClose }) => {
         {/* It also just clicked here; I believe we're gonna FIRSTS filter based on isActive and THEN map the NEW array to use the Note component? */}
       </div>
       <button type='button' onClick={onClose}>Close</button>
+      {/* TODO: Let the esc keyboard button act as an onClose too! */}
     </div>
   )
 }
